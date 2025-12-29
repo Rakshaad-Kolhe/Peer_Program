d956384ff71ef5e131154c18c7ebd5018cba7f94
Rakshaad Kolhe <rakshaadkolhe@gmail.com>
2025-12-29 11:27:43 +0530

    Add files via upload

---
+++ atoi.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int myAtoi(string s) {
+    int i = 0, n = s.length();
+    
+    while (i < n && s[i] == ' ') {
+        i++;
+    }
+    
+    int sign = 1;
+    if (i < n && (s[i] == '+' || s[i] == '-')) {
+        if (s[i] == '-') sign = -1;
+        i++;
+    }
+
+    long long num = 0;
+    while (i < n && s[i] >= '0' && s[i] <= '9') {
+        num = num * 10 + (s[i] - '0');
+
+        if (sign == 1 && num > INT_MAX)
+            return INT_MAX;
+        if (sign == -1 && -num < INT_MIN)
+            return INT_MIN;
+
+        i++;
+    }
+
+    return sign * num;
+}
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    cout << myAtoi(s);
+    return 0;
+}
